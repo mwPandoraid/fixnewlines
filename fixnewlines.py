76bf1e9f5e2c1c9472b3659db67a85bf9f1c571c
Miłosz Skrzypek <miloszskrzypek239@gmail.com>
2023-08-06 16:53:31 +0200

    feat: remove first line if line equals '\n'

--- fixnewlines.py
+++ fixnewlines.py
@@ -21,7 +21,7 @@ def get_filelist(path_to_dir):
     for file in os.listdir(path_to_dir):
         if os.path.isfile(path_to_dir + file):
             filelist.append(path_to_dir + file)
-    
+
     return filelist
 
 
@@ -33,8 +33,9 @@ def get_dirlist(path_to_dir):
 
     return dirlist
 
+
 def has_trailing_newlines(path_to_file):
-    if(path_to_file.split('.')[1] in ignored_extensions):
+    if (path_to_file.split('.')[1] in ignored_extensions):
         print(f"found ignored file extension {path_to_file.split('.')[1]}")
         return False
     with open(path_to_file, 'r') as f:
@@ -42,14 +43,20 @@ def has_trailing_newlines(path_to_file):
         last_line_has_newline = lines[-1] == '\n'
         second_to_last_line_has_newline = lines[-2] == '\n'
         has_trailing = last_line_has_newline and second_to_last_line_has_newline
-        try:    
+        try:
             return has_trailing
         except IndexError:
             return False
-    
-   
+
+
+def starts_with_newline(path_to_file):
+    with open(path_to_file, 'r') as f:
+        lines = f.readlines()
+        return lines[0] == '\n'
+
+
 def add_newline(path_to_file):
-    if(path_to_file.split('.')[1] in ignored_extensions):
+    if (path_to_file.split('.')[1] in ignored_extensions):
         print("found ignored file extension")
     else:
         with open(path_to_file, 'a') as f:
@@ -58,31 +65,47 @@ def add_newline(path_to_file):
 
 
 def has_no_newline(path_to_file):
-    if(path_to_file.split('.')[1] in ignored_extensions):
+    if (path_to_file.split('.')[1] in ignored_extensions):
         print(f"found ignored file extension {path_to_file.split('.')[1]}")
         return True
     with open(path_to_file, 'r') as f:
         lines = f.read()
         has_no_newline = lines[-1] != '\n' and lines[-1] != ' '
-        try:    
+        try:
             return has_no_newline
         except IndexError:
             return False
 
+
 def has_whitespace(path_to_file):
-    if(path_to_file.split('.')[1] in ignored_extensions):
+    if (path_to_file.split('.')[1] in ignored_extensions):
         print(f"found ignored file extension {path_to_file.split('.')[1]}")
         return True
     with open(path_to_file, 'r') as f:
         lines = f.read()
         has_whitespace = lines[-1] == ' ' and lines[-1] != '\n'
-        try:    
+        try:
             return has_whitespace
         except IndexError:
             return False
 
+
+def remove_first_char(path_to_file):
+    if (path_to_file.split('.')[1] in ignored_extensions):
+        print("found ignored file extension")
+    else:
+        with open(path_to_file, 'r', newline='') as f:
+            contents = f.read()
+            f.close()
+            strlist = list(contents)
+            strlist[0] = ""
+            result_string = ''.join(strlist)
+        with open(path_to_file, 'w', newline='') as f:
+            f.write(result_string)
+
+
 def remove_last_char(path_to_file):
-    if(path_to_file.split('.')[1] in ignored_extensions):
+    if (path_to_file.split('.')[1] in ignored_extensions):
         print("found ignored file extension")
     else:
         with open(path_to_file, 'r', newline='') as f:
@@ -94,9 +117,12 @@ def remove_last_char(path_to_file):
         with open(path_to_file, 'w', newline='') as f:
             f.write(result_string)
 
+
 def iterate(path_to_dir):
     for file in get_filelist(path_to_dir):
         try:
+            if starts_with_newline(file):
+                remove_first_char(file)
             if has_no_newline(file):
                 add_newline(file)
             elif has_whitespace(file):
@@ -107,5 +133,6 @@ def iterate(path_to_dir):
             print(f"Failed on file {file}. Traceback: \n{e}")
     for dir in get_dirlist(path_to_dir):
         iterate(dir + '/')
+
 
 iterate(os.path.abspath(parser.parse_args().dir) + '/')
